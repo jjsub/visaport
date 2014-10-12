75d44689092b5019a15112212121fb9a1b40171c
David D. Boling <daviddboling@gmail.com>
2014-10-11 20:56:55 -0500

    Fixed error with random image

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -2,14 +2,8 @@
   'use strict';
 
   angular.module('visaPort')
-  .controller('HomeCtrl', ['$scope', '$interval', 'Home', function($scope, $interval, Home){
+  .controller('HomeCtrl', ['$scope', 'Home', function($scope, Home){
     Home.getMessage().then(function(response){
-      $scope.mean = response.data.mean;
-
-      $interval(function(){
-        $scope.mean = _.shuffle($scope.mean);
-      }, 100);
-    });
   }]);
 })();
 
